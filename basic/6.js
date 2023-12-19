c891398b16860102a547ef75893622b1fc8e91b2
harsh dodiwan <148644825+harshdodiwan@users.noreply.github.com>
2023-12-19 15:42:34 +0000

    container config done

---
+++ basic/6.js
@@ -0,0 +1,26 @@
+let score=33
+console.log(typeof score)
+
+let scores = "45"
+console.log( typeof scores)  
+// its srting bcz of double quotes
+
+let scoress = true
+console.log (typeof scoress)
+
+let scoresss = 1
+console.log (typeof scoresss)
+
+let scoressss = 0
+let booleanscoressss= Boolean (scoressss) 
+console.log ( booleanscoressss);
+
+// isme number to boolean operations change hue h 
+
+
+let number = 0
+let stringnumber= String (number) 
+console.log ( stringnumber);
+console.log (  typeof stringnumber);
+
+// isme number to string operation use hua h 
