55877331d6863e3e323a953e2f87d853bbfae9b8
harsh dodiwan <148644825+harshdodiwan@users.noreply.github.com>
2023-12-19 14:28:55 +0000

    container config done

--- basic/2.js
+++
@@ -1,1 +0,0 @@
-console.log("abe ja na ")
---
+++ basic/var.js
@@ -0,0 +1,19 @@
+const accountId = 14431
+let acccountEmail = "harshdodiwan@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState
+
+// semicolon ; lgao na mt lgao kuch farq ni pdta js main 
+// agr kisi ki value nhi daloge toh output m undefined show hoga jaise is waqt accountsate m ho ra h humne isme iski value nhi di h isiliye 
+
+// accountId = 2 ( agr const lga hua hb toh dubara uski value change nhi hogi 
+// accoutn id ko agr double quotes ke andar dalega toh accountid print hoga na ki uski value 
+// but agr sirf accountid dalega tb value print hogi 
+console. log(accountId);
+
+accountPassword = "444444"
+
+console.table([accountId, accountPassword,acccountEmail, accountState]);
+
+
