1ddedae9b1253cc020f2164ec905d004c76729e9
harsh dodiwan <148644825+harshdodiwan@users.noreply.github.com>
2023-12-20 18:29:29 +0000

    container config done

--- basic/9.js
+++ basic/9.js
@@ -3,3 +3,32 @@
 
 // non-primitive (refernces)
 // aaray , objects, functions
+
+
+// const Id = symbol ('123')
+// const anotherId = symbol ('123')
+
+// console.log (Id === anotherId  );
+
+
+const scorevalue = false
+const bigNumber = 123456565656n
+const size = null 
+let bowl ;
+
+
+const heroes = [" shaktiman" ," dero" , "ndond"]
+
+
+
+
+console.log (typeof bigNumber)
+console.log (typeof scorevalue)
+console.log (typeof size)
+console.log (typeof bowl)
+console.log (typeof heroes)
+
+
+
+
+// null type of is object 
