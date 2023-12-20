20a8666e3a32a431f99373d295f12861e7e6d1a6
harsh dodiwan <148644825+harshdodiwan@users.noreply.github.com>
2023-12-20 19:50:47 +0000

    container config done

---
+++ basic/11.js
@@ -0,0 +1,27 @@
+// string in js 
+
+name = "harsh"
+surname =  " dodiwan "
+
+console.log(name + surname)
+
+// console.log ( my ${name }  ${surname} )
+
+const game = new String ("scorpio" )
+
+console.log ( game [0])
+console.log( game.length)
+console.log( game.charAt('5'))
+console.log( game.indexOf("i"))
+console.log( game.indexOf("r"))
+
+console.log( game.substring(0,4))
+// substring is used to divide the string 
+console.log( game.substring(2,7))
+
+
+const cars = ("     vvubhvfyjgu      ")
+console.log( cars)
+console.log (cars.trim())
+
+// trim delete the spaces
