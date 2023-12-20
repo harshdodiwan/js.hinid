70e60dad58a6f0464a2496db6e979226b8d9b632
harsh dodiwan <148644825+harshdodiwan@users.noreply.github.com>
2023-12-20 21:09:09 +0000

    container config done

---
+++ basic/12.js
@@ -0,0 +1,33 @@
+// numbers and math in js 
+
+const numbeR = 400
+console.log(numbeR)
+console.log(typeof numbeR)
+
+balance =  new Number(500)
+console.log(balance)
+
+// agr hum new number likenge toh wo hume specified krke dikhayega ki ya number hb      
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toString().substring(0,2))
+
+
+value = 1000000
+console.log(value.toLocaleString())
+console.log(value.toLocaleString('en-IN'))
+
+// 'en-IN' mtlb zeroes indians value m split honge mtlb jaise ikai, dhai, shekda, hazzar
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// math
+
+
+console.log(Math)
+console.log(Math.abs(-7))
+console.log(Math.round(456.89))
+console.log(Math.min(456.89,456,67,23,))
+console.log(Math.max(4,45,687,3423))
