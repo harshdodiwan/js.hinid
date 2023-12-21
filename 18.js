e66ce2508c98a4b54dfc31127f02a54d8c54d640
harsh dodiwan <148644825+harshdodiwan@users.noreply.github.com>
2023-12-21 18:53:00 +0000

    container config done

---
+++ 18.js
@@ -0,0 +1,12 @@
+// object de-structure 
+
+const course = {
+     name : "js codecha",
+     price: "999",
+     owner: "hitesh"
+}
+
+// course.owner
+console.log(course.name)
+
+console.log(DINFIRNF)
